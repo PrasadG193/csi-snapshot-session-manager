034d4f7fbaf23a061495d7e452aaaa2481971d23
Prasad Ghangal <prasad.ghangal@gmail.com>
2023-04-24 14:46:28 +0530

    Add authz checks in webhook handler
    Signed-off-by: Prasad Ghangal <prasad.ghangal@gmail.com>

--- go.mod
+++ go.mod
@@ -7,14 +7,15 @@ replace github.com/container-storage-interface/spec v1.7.0 => github.com/PrasadG
 require (
 	github.com/container-storage-interface/spec v1.7.0
 	github.com/go-logr/logr v1.2.3
-	github.com/google/uuid v1.1.2
 	github.com/kubernetes-csi/csi-lib-utils v0.13.0
 	github.com/onsi/ginkgo/v2 v2.6.0
 	github.com/onsi/gomega v1.24.1
 	google.golang.org/grpc v1.49.0
 	google.golang.org/protobuf v1.28.1
-	k8s.io/apimachinery v0.26.0
-	k8s.io/client-go v0.26.0
+	k8s.io/api v0.26.3
+	k8s.io/apimachinery v0.26.3
+	k8s.io/apiserver v0.26.3
+	k8s.io/client-go v0.26.3
 	sigs.k8s.io/controller-runtime v0.14.1
 )
 
@@ -36,6 +37,7 @@ require (
 	github.com/google/gnostic v0.5.7-v3refs // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/google/gofuzz v1.1.0 // indirect
+	github.com/google/uuid v1.1.2 // indirect
 	github.com/imdario/mergo v0.3.6 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -65,9 +67,8 @@ require (
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	k8s.io/api v0.26.0 // indirect
 	k8s.io/apiextensions-apiserver v0.26.0 // indirect
-	k8s.io/component-base v0.26.0 // indirect
+	k8s.io/component-base v0.26.3 // indirect
 	k8s.io/klog/v2 v2.80.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20221012153701-172d655c2280 // indirect
 	k8s.io/utils v0.0.0-20221128185143-99ec85e7a448 // indirect
--- pkg/api/cbt/v1alpha1/volumesnapshotdeltatoken_webhook.go
+++ pkg/api/cbt/v1alpha1/volumesnapshotdeltatoken_webhook.go
@@ -18,10 +18,15 @@ package v1alpha1
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"net/http"
 
 	admissionv1 "k8s.io/api/admission/v1"
+	v1 "k8s.io/api/authorization/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	runtime "k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes"
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
@@ -33,23 +38,128 @@ var volumesnapshotdeltatokenlog = logf.Log.WithName("volumesnapshotdeltatoken-re
 
 func (r *VolumeSnapshotDeltaToken) SetupWebhookWithManager(mgr ctrl.Manager) error {
 	volumesnapshotdeltatokenlog.Info("Registering Webhook handler.")
+	validator := &VolumeSnapshotDeltaTokenValidator{}
 	whServer := mgr.GetWebhookServer()
 	// TODO: Declare as const
-	whServer.Register("/validate-cbt-storage-k8s-io-v1alpha1-volumesnapshotdeltatoken", &webhook.Admission{Handler: &VolumeSnapshotDeltaToken{}})
+	whServer.Register("/validate-cbt-storage-k8s-io-v1alpha1-volumesnapshotdeltatoken", &webhook.Admission{Handler: validator})
 	whServer.CertDir = "/tmp/k8s-webhook-server/serving-certs/"
 	whServer.Port = 9443
+
+	decoder, err := admission.NewDecoder(mgr.GetScheme())
+	if err != nil {
+		return err
+	}
+	validator.decoder = decoder
+	cfg, err := ctrl.GetConfig()
+	if err != nil {
+		return err
+	}
+	kubeClient, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		return err
+	}
+	validator.cli = kubeClient
 	return nil
 }
 
-// +kubebuilder:webhook:path=/validate-cbt-storage-k8s-io-v1alpha1-volumesnapshotdeltatoken,mutating=false,failurePolicy=fail,sideEffects=None,groups=cbt.storage.k8s.io,resources=volumesnapshotdeltatokens,verbs=create;update,versions=v1alpha1,name=vvolumesnapshotdeltatoken.kb.io,admissionReviewVersions=v1
-var _ admission.Handler = &VolumeSnapshotDeltaToken{}
+var _ admission.Handler = &VolumeSnapshotDeltaTokenValidator{}
 
-func (a *VolumeSnapshotDeltaToken) Handle(ctx context.Context, req admission.Request) admission.Response {
+// +kubebuilder:webhook:path=/validate-cbt-storage-k8s-io-v1alpha1-volumesnapshotdeltatoken,mutating=true,failurePolicy=fail,sideEffects=None,groups=cbt.storage.k8s.io,resources=volumesnapshotdeltatokens,verbs=create;update,versions=v1alpha1,name=vvolumesnapshotdeltatoken.kb.io,admissionReviewVersions=v1
+// +kubebuilder:object:generate=false
+type VolumeSnapshotDeltaTokenValidator struct {
+	decoder *admission.Decoder
+	cli     kubernetes.Interface
+}
+
+func (v *VolumeSnapshotDeltaTokenValidator) Handle(ctx context.Context, req admission.Request) admission.Response {
 	fmt.Printf("USERINFO::: %#v\n", req.UserInfo)
-	return admission.Response{
-		AdmissionResponse: admissionv1.AdmissionResponse{
-			Allowed: true,
-			Result:  &metav1.Status{},
+	// Do the validation
+	vsd := &VolumeSnapshotDeltaToken{}
+	err := v.decoder.Decode(req, vsd)
+	if err != nil {
+		volumesnapshotdeltatokenlog.Error(err, "Failed to decode request object")
+		return admission.Errored(http.StatusBadRequest, err)
+	}
+
+	// Verify if the user has permission to GET volumesnapshot resources
+	//userInfo, err := userToInfo(req.UserInfo)
+	//if err != nil {
+	//	volumesnapshotdeltatokenlog.Error(err, "Failed to decode request object")
+	//	return admission.Errored(http.StatusBadRequest, err)
+	//}
+	//att :=
+	//fmt.Println(att)
+	extra := make(map[string]v1.ExtraValue, len(req.UserInfo.Extra))
+	for u, e := range req.UserInfo.Extra {
+		extra[u] = v1.ExtraValue(e)
+	}
+
+	sar := &v1.SubjectAccessReview{
+		Spec: v1.SubjectAccessReviewSpec{
+			ResourceAttributes: GetResourceAttributes(req.Namespace),
+			User:               req.UserInfo.Username,
+			Groups:             req.UserInfo.Groups,
+			Extra:              extra,
+			UID:                req.UserInfo.UID,
 		},
+	}
+	sarResp, err := v.cli.AuthorizationV1().SubjectAccessReviews().Create(ctx, sar, metav1.CreateOptions{})
+	if err != nil {
+		return admission.Errored(http.StatusBadRequest, err)
+	}
+	//sarJson, err := sarResp.Status.Marshal()
+	//fmt.Printf("debug: SAR Response:: %s %v\n", string(sarJson), err)
+	if !sarResp.Status.Allowed || sarResp.Status.Denied {
+		return admission.Response{
+			AdmissionResponse: admissionv1.AdmissionResponse{
+				Allowed: false,
+				Result:  &metav1.Status{},
+			},
+		}
+	}
+	vsd.Status.SessionState = SessionStateTypePending
+	marshaledObject, err := json.Marshal(runtime.Object(vsd))
+	if err != nil {
+		return admission.Errored(http.StatusBadRequest, err)
+	}
+	fmt.Println("debug: webhook req object Raw:: ", string(req.Object.Raw))
+	fmt.Println("debug: webhook result:: ", string(marshaledObject))
+	patched := admission.PatchResponseFromRaw(req.Object.Raw, marshaledObject)
+	fmt.Printf("debug: webhook patched response:: %#v\n", patched)
+	return patched
+}
+
+//// TODO: Find if there is better way to conver auth.UserInfo to user.Info
+//func userToInfo(userInfo v1.UserInfo) (user.Info, error) {
+//	//b, err := userInfo.Marshal()
+//	//if err != nil {
+//	//	return nil, err
+//	//}
+//	//ui := user.DefaultInfo{}
+//	//err = json.Unmarshal(b, &ui)
+//	//return &ui, err
+//	defInfo := &user.DefaultInfo{
+//		Name:   userInfo.Username,
+//		UID:    userInfo.UID,
+//		Groups: userInfo.Groups,
+//		Extra:  make(map[string]string),
+//	}
+//	for k, v := range userInfo.Extra {
+//		defInfo.Extra[k] = string(v)
+//	}
+//	return defInfo, nil
+//}
+
+func GetResourceAttributes(namespace string) *v1.ResourceAttributes {
+	apiVerb := "get"
+	vsGroup := "snapshot.storage.k8s.io"
+	vsVersion := "v1"
+	vsResource := "volumesnapshots"
+	return &v1.ResourceAttributes{
+		Verb:      apiVerb,
+		Namespace: namespace,
+		Group:     vsGroup,
+		Version:   vsVersion,
+		Resource:  vsResource,
 	}
 }
